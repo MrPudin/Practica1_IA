7fee135ce705101d2e5cac53dcaa7e3013de8976
MrPudin <daranutacristian60@gmail.com>
2025-10-27 20:56:28 +0100

    IDS implemented

--- algorithms/ids.py
+++ algorithms/ids.py
@@ -1,35 +1,35 @@
 from hlogedu.search.algorithm import Algorithm, Node, Solution
-from hlogedu.search.containers import Queue
+from hlogedu.search.containers import Stack
 
 """Codigo Ejemplo para hacer luego el IDS"""
-class TreeBfs(Algorithm):
-    NAME = "my-tree-bfs"
+class TreeIDS(Algorithm):
+    NAME = "my-tree-ids"
 
-    def __init__(self, problem):
-        super().__init__(problem)
-        self.fringe = Queue()
+    def __init__(self, problem, max_depth=50):
+        super().__init__(problem) 
+        self.max_depth = max_depth
 
     def run(self):
         expand_counter = 0
         roots = [Node(s) for s in self.problem.get_start_states()]
 
-        for n in roots:
-            if self.problem.is_goal_state(n.state):
-                return Solution(self.problem, roots, solution_node=n)
-            self.fringe.push(n)
+        for depth_limit in range(self.max_depth):
+            self.fringe = Stack()
+            for n in roots:
+                self.fringe.push((n, 0))
 
-        while self.fringe:
-            n = self.fringe.pop()
-            expand_counter += 1
-            n.expand_order = expand_counter
-            # for s, a, c in self.problem.get_successors(n.state):
-            for s, a, c in sorted(
-                self.problem.get_successors(n.state), key=lambda x: x[0]
-            ):
-                ns = Node(s, a, cost=n.cost + c, parent=n)
-                n.add_successor(ns)
-                if self.problem.is_goal_state(ns.state):
-                    return Solution(self.problem, roots, solution_node=ns)
-                self.fringe.push(ns)
+            while self.fringe:
+                n, depth = self.fringe.pop()
+                expand_counter += 1
+                n.expand_order = expand_counter
+                
+                if self.problem.is_goal_state(n.state):
+                    return Solution(self.problem, roots, solution_node=n)
+                
+                if depth < depth_limit:
+                    for s, a, c in sorted(self.problem.get_successors(n.state), key=lambda x: x[0]):
+                        ns = Node(s, a, cost=n.cost + c, parent=n)
+                        n.add_successor(ns)
+                        self.fringe.push((ns, depth+1))
 
         return Solution(self.problem, roots)
